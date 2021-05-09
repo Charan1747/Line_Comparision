fa7fa3747759807d1a0b7de879e5f88609e8f2c0
Ketha Charan <charan.ketha1747@gmail.com>
2021-05-09 15:25:37 -0700

    [Charan] Add : Checking Two lines Are Equal or Not

--- LineComparisionProblems/LineComparisionProblems/Program.cs
+++ LineComparisionProblems/LineComparisionProblems/Program.cs
@@ -4,17 +4,43 @@ namespace LineComparisionProblems
 {
     class Program
     {
+        int X1, X2, Y1, Y2;
+        double Length;
+        public void Coordinates()
+        {
+            Console.WriteLine("Enter your X1,X2,Y1,Y2:");
+            X1 = Convert.ToInt32(Console.ReadLine());
+            X2 = Convert.ToInt32(Console.ReadLine());
+            Y1 = Convert.ToInt32(Console.ReadLine());
+            Y2 = Convert.ToInt32(Console.ReadLine());
+        }
+
+        public double CalCulate(int X1, int X2, int Y1, int Y2)
+        {
+            Length = Math.Sqrt(Math.Pow((X2 - X1), 2) + Math.Pow((Y2 - Y1), 2));
+            return Length;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter The Values of X1,X2,Y1,Y2:");
-            int X1 = Convert.ToInt32(Console.ReadLine());
-            int X2 = Convert.ToInt32(Console.ReadLine());
-            int Y1 = Convert.ToInt32(Console.ReadLine());
-            int Y2 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("X1 is: " + X1 + "\t X2 is: " + X2 + "\tY1 is: " + Y1 + "\tY2 is: " + Y2);
-            double Length = Math.Sqrt(Math.Pow((X2 - X1), 2) + Math.Pow((Y2 - Y1), 2));
-            Console.WriteLine("Your Length is: " + Length);
-            Console.ReadKey();
+            Console.WriteLine("Line Comparision Problem");
+            Program lobject = new Program();
+            Console.WriteLine("Enter The First Line Co-Ordinates: ");
+            lobject.Coordinates();
+            lobject.CalCulate(lobject.X1, lobject.X2, lobject.Y1, lobject.Y2);
+            double Length_of_First_Line = lobject.Length;
+            Console.WriteLine("Enter The Second Line Co-Ordinates: ");
+            lobject.Coordinates();
+            lobject.CalCulate(lobject.X1, lobject.X2, lobject.Y1, lobject.Y2);
+            double Length_of_Second_Line = lobject.Length;
+
+            if (Length_of_First_Line == Length_of_Second_Line)
+            {
+                Console.WriteLine("Two lines are Equal");
+            }
+            else
+            {
+                Console.WriteLine("Two lines are Not Equal");
+            }
         }
     }
 }
